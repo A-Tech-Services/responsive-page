94aa9a1ec5c080ccc5d7d7f40267c974b8448aab
A-Tech-Services <abiodunoluwapelumi859@gmail.com>
2022-10-07 03:21:15 +0100

    update

--- Js/script.js
+++ Js/script.js
@@ -1,25 +1,10 @@
-let Hamburger = document.querySelector(".hamburger");
-let HamburgerActive = document.querySelector(".hamburgeractive");
-let NavMenu = document.querySelector(".nav-menu");
-let NavItem = Array.from(document.getElementsByClassName("nav-link"));
+// reference
+let NavItem = Array.from(document.getElementsByClassName("nav-item"));
 
-
-Hamburger.addEventListener("click", () => {
-    NavMenu.classList.add("active");
-    HamburgerActive.classList.add("active");
-    Hamburger.classList.add("down");
-})
-
-HamburgerActive.addEventListener("click", () => {
-    NavMenu.classList.remove("active");
-    HamburgerActive.classList.remove("active");
-    Hamburger.classList.remove("down");
-})
-
-NavItem.forEach(menu => {
-    menu.addEventListener("click", () => {
-        NavMenu.classList.remove("active");
-        HamburgerActive.classList.remove("active");
-        Hamburger.classList.remove("down");
-    })
-})
+NavItem.forEach(nav =>{
+    nav.addEventListener("click", () =>{
+        let navBarClick = document.querySelector(".active");
+        navBarClick?.classList.remove("active");
+        nav.classList.add("active");
+    });
+});
