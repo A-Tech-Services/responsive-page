380049d5e8a699bb20f35c79c1150863de140f54
A-Tech-Services <abiodunoluwapelumi859@gmail.com>
2022-10-10 10:34:46 +0100

    update

--- Js/script.js
+++ Js/script.js
@@ -1,5 +1,13 @@
 // reference
 let NavItem = Array.from(document.getElementsByClassName("nav-item"));
+let frontEnd = document.querySelector(".frontend");
+let uiux = document.querySelector(".uiux");
+let graphics = document.querySelector(".graphics");
+let allFolio = document.querySelector(".allfolio");
+let Menu1 = document.querySelector(".con1");
+let Menu2 = document.querySelector(".con2");
+let Menu3 = document.querySelector(".con3");
+let Menu4 = document.querySelector(".con4");
 
 NavItem.forEach(nav =>{
     nav.addEventListener("click", () =>{
@@ -8,3 +16,32 @@ NavItem.forEach(nav =>{
         nav.classList.add("active");
     });
 });
+
+
+Menu1.addEventListener("click", ()=>{
+    allFolio.classList.add("active");
+    uiux.classList.remove("active");
+    frontEnd.classList.add("down");
+    graphics.classList.remove("active");
+})
+
+Menu2.addEventListener("click", ()=>{
+    allFolio.classList.remove("active");
+    uiux.classList.remove("active");
+    frontEnd.classList.remove("down");
+    graphics.classList.remove("active");
+})
+
+Menu3.addEventListener("click", ()=>{
+    allFolio.classList.remove("active");
+    uiux.classList.add("active");
+    frontEnd.classList.add("down");
+    graphics.classList.remove("active");
+})
+
+Menu4.addEventListener("click", ()=>{
+    allFolio.classList.remove("active");
+    uiux.classList.remove("active");
+    frontEnd.classList.add("down");
+    graphics.classList.add("active");
+})
